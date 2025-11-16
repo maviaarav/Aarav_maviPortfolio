e85efed1d03544abc82591ad183f0253bb9bf350
maviaarav <76863907+maviaarav@users.noreply.github.com>
2025-11-16 18:45:35 +0530

    p fixed

--- src/about.jsx
+++ src/about.jsx
@@ -7,7 +7,7 @@ function About() {
         <div className="about-page">
             <div className='about-text'>
             <h1>About Me</h1>
-            <p>
+            <p className='p'>
             I'm a passionate designer with a focus on creating intuitive and engaging user experiences. With a background in visual communication and a keen eye for detail, I strive to bring creativity and functionality together in every project.
             </p>
             <h3 className='h3'>I'm a young student currently in 9th grade, balancing schoolwork with my passion for coding. Over the past three years, I've explored various areas of development, including front-end, backend, and UI design. I work with HTML, CSS, JavaScript, Python, and React, continually learning to sharpen my skills. I'm dedicated to growing in this field, taking on new challenges, and building creative, functional digital experiences.</h3>
